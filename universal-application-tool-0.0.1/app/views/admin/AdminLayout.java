db539bc19102a6685313bc184ef8dcaa387824d2
AWBaumann <77127160+AWBaumann@users.noreply.github.com>
2021-03-05 11:07:22 -0800

    I like this one

--- universal-application-tool-0.0.1/app/views/admin/AdminLayout.java
+++ universal-application-tool-0.0.1/app/views/admin/AdminLayout.java
@@ -2,15 +2,18 @@ package views.admin;
 
 import static j2html.TagCreator.a;
 import static j2html.TagCreator.body;
+import static j2html.TagCreator.div;
 import static j2html.TagCreator.head;
-import static j2html.TagCreator.header;
 import static j2html.TagCreator.main;
+import static j2html.TagCreator.nav;
 
 import j2html.tags.ContainerTag;
 import j2html.tags.DomContent;
 import javax.inject.Inject;
 import play.twirl.api.Content;
 import views.BaseHtmlLayout;
+import views.StyleUtils;
+import views.Styles;
 import views.ViewUtils;
 
 public class AdminLayout extends BaseHtmlLayout {
@@ -22,14 +25,36 @@ public class AdminLayout extends BaseHtmlLayout {
 
   /** Renders mainDomContents within the main tag, in the context of the admin layout. */
   public Content render(DomContent... mainDomContents) {
+    ContainerTag subWrapper =
+        div(renderAdminHeader(), main(mainDomContents))
+            .withClasses("flex flex-col flex-1 h-full overflow-hidden");
+    ContainerTag mainWrapper = div(subWrapper).withClasses("flex h-screen overflow-y-hidden");
+    return htmlContent(head(tailwindStyles()), body(mainWrapper));
+  }
+
+  public ContainerTag renderAdminHeader() {
+    String questionTitle = "Questions";
     String questionLink = controllers.admin.routes.QuestionController.index("table").url();
+    String programTitle = "Programs";
     String programLink = controllers.admin.routes.AdminProgramController.index().url();
+    String logoutTitle = "Logout";
     String logoutLink = org.pac4j.play.routes.LogoutController.logout().url();
+
     ContainerTag adminHeader =
-        header()
-            .with(a("Questions").withHref(questionLink))
-            .with(a("Programs").withHref(programLink))
-            .with(a("Logout").withHref(logoutLink));
-    return htmlContent(head(tailwindStyles()), body(adminHeader, main(mainDomContents)));
+        nav()
+            .withClasses(
+                Styles.BG_GRAY_800, Styles.TEXT_GRAY_200, Styles.H_12, Styles.PX_4, Styles.PY_3)
+            .with(
+                a(questionTitle)
+                    .withClasses(Styles.PX_3, StyleUtils.hover(Styles.TEXT_WHITE))
+                    .withHref(questionLink),
+                a(programTitle)
+                    .withClasses(Styles.PX_3, StyleUtils.hover(Styles.TEXT_WHITE))
+                    .withHref(programLink),
+                a(logoutTitle)
+                    .withClasses(
+                        Styles.PX_3, StyleUtils.hover(Styles.TEXT_WHITE), Styles.FLOAT_RIGHT)
+                    .withHref(logoutLink));
+    return adminHeader;
   }
 }
--- universal-application-tool-0.0.1/app/views/admin/programs/ProgramBlockEditView.java
+++ universal-application-tool-0.0.1/app/views/admin/programs/ProgramBlockEditView.java
@@ -1,13 +1,9 @@
 package views.admin.programs;
 
 import static com.google.common.base.Preconditions.checkNotNull;
-import static j2html.TagCreator.a;
 import static j2html.TagCreator.div;
-import static j2html.TagCreator.form;
-import static j2html.TagCreator.h1;
-import static j2html.TagCreator.h2;
-import static j2html.TagCreator.li;
-import static j2html.TagCreator.ul;
+import static j2html.TagCreator.p;
+import static j2html.TagCreator.span;
 
 import com.google.common.collect.ImmutableList;
 import com.google.inject.Inject;
@@ -38,139 +34,129 @@ public class ProgramBlockEditView extends BaseHtmlView {
       ImmutableList<QuestionDefinition> questions) {
     Tag csrfTag = makeCsrfTokenInputTag(request);
 
-    return layout.render(
-        title(program),
-        topButtons(csrfTag, program),
+    String str = block.name() + questions.toString();
+    Tag junkDiv = div(csrfTag, span(str)).withClasses(Styles.HIDDEN);
+
+    return layout.render(programInfo(program), programContent(program), junkDiv);
+  }
+
+  private Tag programInfo(ProgramDefinition program) {
+    ContainerTag programStatus =
+        div("Draft").withId("program-status").withClasses(Styles.TEXT_XS, Styles.UPPERCASE);
+    ContainerTag programTitle =
+        div(program.name()).withId("program-title").withClasses(Styles.TEXT_3XL, Styles.PB_3);
+    ContainerTag programDescription = div(program.description()).withClasses(Styles.TEXT_SM);
+
+    ContainerTag programInfo =
+        div(programStatus, programTitle, programDescription)
+            .withId("program-info")
+            .withClasses(
+                Styles.BG_GRAY_100,
+                Styles.TEXT_GRAY_800,
+                Styles.SHADOW_MD,
+                Styles.P_8,
+                Styles.PT_4);
+
+    return programInfo;
+  }
+
+  private Tag programContent(ProgramDefinition program) {
+    ContainerTag blockEditor =
+        div().withId("block-editor").withClasses(Styles.FLEX, Styles.H_FULL, Styles.RELATIVE);
+
+    ContainerTag programBlocks = renderProgramBlocks(program);
+
+    ContainerTag questionBank =
         div()
-            .withClasses(Styles.FLEX)
-            .with(blockOrderPanel(program, block))
-            .with(blockEditPanel(csrfTag, program, block))
-            .with(questionBankPanel(csrfTag, questions, program, block)));
+            .withId("question-bank")
+            .withClasses(
+                "w-1/3",
+                Styles.RELATIVE,
+                Styles.BORDER_L,
+                Styles.BORDER_GRAY_400,
+                Styles.BG_GRAY_50,
+                Styles.SHADOW_INNER,
+                Styles.TEXT_GRAY_600,
+                Styles.P_4);
+
+    return blockEditor.with(programBlocks, questionBank);
   }
 
-  private Tag title(ProgramDefinition program) {
-    return h1(program.name() + " Questions");
+  private ContainerTag renderProgramBlocks(ProgramDefinition program) {
+    ContainerTag programBlocks = div().withId("program-blocks").withClasses("w-2/3", Styles.P_6);
+    program.blockDefinitions().forEach(block -> programBlocks.with(renderProgramBlock(block)));
+    return programBlocks;
   }
 
-  private ContainerTag topButtons(Tag csrfTag, ProgramDefinition program) {
-    String addBlockUrl =
-        controllers.admin.routes.AdminProgramBlocksController.create(program.id()).url();
-    ContainerTag addBlockForm =
-        form(csrfTag, submitButton("Add Block")).withMethod("post").withAction(addBlockUrl);
+  private ContainerTag renderProgramBlock(BlockDefinition block) {
+    String blockString = "block-" + block.id();
+    ContainerTag programBlock =
+        div()
+            .withId("program-" + blockString)
+            .withClasses(Styles.SHADOW_XL, Styles.ROUNDED_MD, Styles.MB_8);
 
-    return div(addBlockForm);
-  }
+    boolean blockHasQuestions = block.getQuestionCount() > 0;
+    ContainerTag blockData =
+        div()
+            .withId(blockString + "-data")
+            .withClasses(
+                Styles.RELATIVE,
+                Styles.BG_GRAY_400,
+                Styles.TEXT_GRAY_50,
+                Styles.P_4,
+                blockHasQuestions ? Styles.ROUNDED_T_MD : Styles.ROUNDED_MD);
 
-  private ContainerTag blockOrderPanel(ProgramDefinition program, BlockDefinition focusedBlock) {
-    ContainerTag list = ul();
+    ContainerTag blockTitle =
+        div(block.name()).withId(blockString + "-title").withClasses(Styles.MB_1, Styles.UPPERCASE);
 
-    for (BlockDefinition block : program.blockDefinitions()) {
-      String editBlockLink =
-          controllers.admin.routes.AdminProgramBlocksController.edit(program.id(), block.id())
-              .url();
-      ContainerTag link = a().withText(block.name()).withHref(editBlockLink);
+    ContainerTag blockDescription =
+        div(block.description()).withId(blockString + "-description").withClasses(Styles.TEXT_XS);
 
-      if (block.hasSameId(focusedBlock)) {
-        link.withClass(Styles.FONT_BOLD);
-      }
-
-      list.with(li(link));
+    blockData.with(blockTitle, blockDescription);
+    // Delete block.
+    // <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 absolute top-7 right-5" fill="none"
+    // viewBox="0 0 24 24" stroke="currentColor">
+    //   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867
+    // 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1
+    // 1 0 00-1 1v3M4 7h16" />
+    // </svg>
+    if (!blockHasQuestions) {
+        return programBlock.with(blockData);
     }
 
-    return div().withClasses(Styles.FLEX_INITIAL).with(list);
-  }
+    ContainerTag blockQuestions =
+        div()
+            .withId(blockString + "-questions")
+            .withClasses(
+                Styles.BG_WHITE,
+                Styles.BORDER,
+                Styles.BORDER_T_0,
+                Styles.BORDER_GRAY_300,
+                Styles.ROUNDED_B_MD,
+                Styles.TEXT_GRAY_600);
 
-  private ContainerTag blockEditPanel(
-      Tag csrfTag, ProgramDefinition program, BlockDefinition block) {
-    ContainerTag questionList = ul().withId("blockQuestions");
     block
         .programQuestionDefinitions()
         .forEach(
-            pqd ->
-                questionList.with(
-                    li(
-                        checkboxInputWithLabel(
-                            pqd.getQuestionDefinition().getName(),
-                            "block-question-" + pqd.getQuestionDefinition().getId(),
-                            "block-question-" + pqd.getQuestionDefinition().getId(),
-                            String.valueOf(pqd.getQuestionDefinition().getId())))));
+            question -> {
+              ContainerTag questionDiv =
+                  div(
+                      // add type svg
+                      p().withText(question.getQuestionDefinition().getName())
+                          .withClasses(Styles.INLINE, Styles.TEXT_SM))
+                      .withClasses(
+                          "odd:bg-gray-100 border-t border-gray-300 first:border-transparent p-3");
+              blockQuestions.with(questionDiv);
+            });
+    //   <svg class="inline flex-shrink-0 h-6 w-6 mr-1 text-sm" xmlns="http://www.w3.org/2000/svg"
+    // fill="currentColor" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1%"
+    // aria-hidden="true">
+    //     <path xmlns="http://www.w3.org/2000/svg" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10
+    // 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0
+    // 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5
+    // 3.22-6 3.22z" />
+    //   </svg>
 
-    return div()
-        .withClass(Styles.FLEX_AUTO)
-        .with(blockEditPanelTop(csrfTag, program, block))
-        .with(
-            div(
-                form(
-                        csrfTag,
-                        div(textFieldWithValue("name", "Block Name", block.name())),
-                        div(
-                            textFieldWithValue(
-                                "description", "Block Description", block.description())),
-                        submitButton("Update Block"))
-                    .withMethod("post")
-                    .withAction(
-                        controllers.admin.routes.AdminProgramBlocksController.update(
-                                program.id(), block.id())
-                            .url()),
-                form()
-                    .withMethod("post")
-                    .withAction(
-                        controllers.admin.routes.AdminProgramBlockQuestionsController.destroy(
-                                program.id(), block.id())
-                            .url())
-                    .with(csrfTag)
-                    .with(questionList)
-                    .with(submitButton("Remove questions"))));
-  }
-
-  private ContainerTag blockEditPanelTop(
-      Tag csrfTag, ProgramDefinition program, BlockDefinition block) {
-    String deleteBlockLink =
-        controllers.admin.routes.AdminProgramBlocksController.destroy(program.id(), block.id())
-            .url();
-
-    ContainerTag blockEditPanelTop = div().withClass(Styles.FLEX).with(h1(block.name()));
-    if (program.blockDefinitions().size() > 1) {
-      ContainerTag deleteButton =
-          form(csrfTag, submitButton("Delete Block"))
-              .withMethod("post")
-              .withAction(deleteBlockLink);
-      blockEditPanelTop.with(deleteButton);
-    }
-    return blockEditPanelTop;
-  }
-
-  private ContainerTag questionBankPanel(
-      Tag csrfTag,
-      ImmutableList<QuestionDefinition> questionDefinitions,
-      ProgramDefinition program,
-      BlockDefinition block) {
-    ContainerTag questionBank = div().withClasses(Styles.FLEX_INITIAL).with(h2("Question Bank"));
-    ContainerTag form =
-        form()
-            .withMethod("post")
-            .withAction(
-                controllers.admin.routes.AdminProgramBlockQuestionsController.create(
-                        program.id(), block.id())
-                    .url())
-            .with(csrfTag)
-            .with(submitButton("Add to Block"));
-
-    ContainerTag questionList =
-        ul().withId("questionBankQuestions").withClass(Styles.OVERFLOW_X_SCROLL);
-
-    questionDefinitions.stream()
-        .filter(question -> !program.hasQuestion(question))
-        .forEach(
-            questionDefinition ->
-                questionList.with(
-                    li(
-                        checkboxInputWithLabel(
-                            questionDefinition.getName(),
-                            "question-" + questionDefinition.getId(),
-                            "question-" + questionDefinition.getId(),
-                            String.valueOf(questionDefinition.getId())))));
-
-    return questionBank.with(form.with(questionList));
+    return programBlock.with(blockData, blockQuestions);
   }
 }
